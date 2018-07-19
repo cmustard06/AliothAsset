57a1f3f8f66069188bbc84e7e140dc74d6c2d5b0
cmustard06 <cmustard06@gmail.com>
2018-07-19 22:58:40 +0800

    fix bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # __Author__:cmustard
 
+import pymysql
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+
+pymysql.install_as_MySQLdb()
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='@1`qweer,.?123'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:123456@localhost:3306/asset' #这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建的数据库名text1
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:123456@127.0.0.1/asset' #这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建的数据库名text1
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True  # 设置这一项是每次请求结束后都会自动提交数据库中的变动
 
 db = SQLAlchemy(app) #实例化
