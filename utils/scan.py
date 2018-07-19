6d3a1cfc2b41db8da382d85fcc2f0e98527e8443
cmustard06 <cmustard06@gmail.com>
2018-07-19 22:10:49 +0800

    update

--- utils/scan.py
+++ utils/scan.py
@@ -2,11 +2,24 @@
 # __Author__:cmustard
 
 import nmap
+from .model import *
 
 class Nmap(object):
-    pass
+    def __init__(self):
+        self.host = None
+        self.ports = None
+
+    def scan(self,host,ports):
+        nm = nmap.PortScanner()
+        result = nm.scan(hosts=host,ports=ports,arguments="-sS",sudo=True)
+
+
 
 
 
 class Mascan(object):
     pass
+
+if __name__ == '__main__':
+    n = Nmap()
+    n.scan("192.168.199.1","80,443,22")
